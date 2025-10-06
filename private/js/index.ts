a4db7871301f23b78118f52797379eea094f38d5
Chris Strawser <mcochris@users.noreply.github.com>
2025-10-06 04:02:11 -0400

    WIP Major re-write

--- private/js/index.ts
+++ private/js/index.ts
@@ -1,14 +1,19 @@
+declare const DOMPurify: any;
+
+//=============================================================
+// Main script
+//=============================================================
+
 (function () {
-	const PRODUCTION = false;
 	const TIMEZONE = Intl.DateTimeFormat().resolvedOptions().timeZone
-	const DOCUMENT_CSRF_TOKEN = document.querySelector("#csrf-token") as HTMLInputElement;
-	const DOCUMENT_FOOTER_YEAR = document.querySelector("#current-year") as HTMLSpanElement;
+	//const DOCUMENT_CSRF_TOKEN = document.querySelector("#csrf-token") as HTMLInputElement;
+	const DOCUMENT_FOOTER_YEAR = document.querySelector("#current-year") as HTMLSpanElement
 	const DOCUMENT_MAIN = document.querySelector("main") as HTMLElement;
 	const DOCUMENT_FORM = document.querySelector("form") as HTMLFormElement;
 	const DOCUMENT_EMAIL = document.querySelector("#email") as HTMLInputElement;
-	const DOCUMENT_MESSAGE = document.querySelector("#message") as HTMLDivElement;
-	const DOCUMENT_FORM_BUTTON = document.querySelector("#form-button") as HTMLButtonElement;
-	const CLIENT_TOKEN_NAME = "client_token";
+	//const DOCUMENT_FORM_BUTTON = document.querySelector("#form-button") as HTMLButtonElement;
+	//const COOKIE_NAME = "PHPSESSID";
+	const LOADER = document.querySelector(".loader") as HTMLSpanElement;
 
 	// Set current year in page footer
 	DOCUMENT_FOOTER_YEAR.textContent = new Date().getFullYear().toString();
@@ -25,143 +30,134 @@
 	// Function to post data to server
 	//=============================================================
 	async function postData(data: FormData): Promise<ServerResponse> {
-		const RESPONSE = await fetch("api.php", {
+		const response = await fetch("api.php", {
 			method: 'POST',
 			body: data,
+			credentials: 'same-origin', // Important: includes cookies
 			signal: AbortSignal.timeout(5000)
 		});
 
-		const REPLY = await RESPONSE.json();
-		return REPLY as Promise<ServerResponse>;
+		const reply = await response.json();
+		return reply as Promise<ServerResponse>;
 	}
 
 	//=============================================================
 	// Send timezone to server
 	//=============================================================
-	(async function (): Promise<void> {
-		const FORM_DATA = new FormData();
-		FORM_DATA.append('script', "set_tz.php");
-		FORM_DATA.append('timezone', TIMEZONE);
-		const REPLY = await postData(FORM_DATA);
-		if (!REPLY.display) {
-			DOCUMENT_MESSAGE.textContent = "Internal error 1";
-			PRODUCTION || console.error("Error setting timezone:", REPLY.message);
-			DOCUMENT_MAIN.style.visibility = "visible";
-			disableEmailInput();
+	async function sendTimezone(): Promise<void> {
+		const form_data = new FormData();
+		form_data.append('script', "set_tz.php");
+		form_data.append('timezone', TIMEZONE);
+		const reply = await postData(form_data);
+		if (!reply.display) {
+			DOCUMENT_MAIN.innerHTML = DOMPurify.sanitize(reply.message);
+			//DOCUMENT_MAIN.style.visibility = "visible";
+		}
+	};
+
+	//=============================================================
+	// Check if user is already logged in via browser localStorage
+	//=============================================================
+	async function isUserLoggedIn(): Promise<void> {
+		const logged_in_token = localStorage.getItem("logged_in_token");
+		if (!logged_in_token) return;
+
+		// Verify the token with server
+		const loginForm = new FormData();
+		loginForm.append('script', "is_user_logged_in.php");
+		loginForm.append('token', logged_in_token);
+
+		// If token is invalid, user may be logged in but in timeout
+		// Need to display login form again
+		const loginReply = await postData(loginForm);
+
+		if (!loginReply.display) {
+			localStorage.removeItem("logged_in_token");
 			return;
 		}
-	})();
 
-	//=============================================================
-	// Check if user is already logged in
-	//=============================================================
-	async function isUserLoggedIn(): Promise<boolean> {
-		const CLIENT_TOKEN = localStorage.getItem(CLIENT_TOKEN_NAME);
-		if (!CLIENT_TOKEN)
-			return Promise.resolve(false);
-
-		// If we get to here, we have a client token in localstorage, now verify it
-		const FORM_DATA = new FormData();
-		FORM_DATA.append('script', "is_user_logged_in.php");
-		FORM_DATA.append('client_token', CLIENT_TOKEN);
-
-		const REPLY = await postData(FORM_DATA);
-		if (REPLY.display) {
-			localStorage.setItem(CLIENT_TOKEN_NAME, REPLY.message); // Store new client token in localstorage
-			return Promise.resolve(true);
-		} else {
-			localStorage.removeItem(CLIENT_TOKEN_NAME);
-			return Promise.resolve(false);
+		if (loginReply.display && loginReply.message) {
+			// User is logged, display message (may be error or info)
+			DOCUMENT_MAIN.innerHTML = DOMPurify.sanitize(loginReply.message);
+			//DOCUMENT_MAIN.style.visibility = "visible";
+			return;
 		}
+
+		// User is logged in, fetch member area
+		await displayMemberArea();
 	}
-
-	isUserLoggedIn().then(loggedIn => {
-		// If user is logged in, redirect to member area
-		if (loggedIn) {
-			(async () => {
-				const FORM_DATA = new FormData();
-				FORM_DATA.append('script', "members.php");
-				FORM_DATA.append('token', localStorage.getItem(CLIENT_TOKEN_NAME) || "");
-				const REPLY = await postData(FORM_DATA);
-				if (REPLY.display) {
-					DOCUMENT_MAIN.innerHTML = REPLY.message;	//	will be the members area HTML
-					DOCUMENT_MAIN.style.visibility = "visible";
-				} else
-					getCSRFToken();
-			})().catch(error => {
-				PRODUCTION || console.error("Error fetching members.php:", error);
-			});
-			return;
-		}
-	});
 
 	//=============================================================
 	// Check if user clicked on login link in email
 	//=============================================================
-	async function isEmailTokenValid(): Promise<boolean> {
-		const URL_PARAMS = new URLSearchParams(window.location.search);
-		const TOKEN = URL_PARAMS.get('token');
-		if (!TOKEN)
-			return Promise.resolve(false);
+	async function isEmailTokenValid(): Promise<void> {
+		const url_params = new URLSearchParams(window.location.search);
+		const email_token = url_params.get('token');
+		if (!email_token) return;
 
-		const FORM_DATA = new FormData();
-		FORM_DATA.append('script', "is_email_token_valid.php");
-		FORM_DATA.append('token', TOKEN);
+		const form_data = new FormData();
+		form_data.append('script', "is_email_token_valid.php");
+		form_data.append('token', email_token);
 
-		const REPLY = await postData(FORM_DATA);
-		if (REPLY.display) {
-			localStorage.setItem(CLIENT_TOKEN_NAME, REPLY.message); // Store new session ID in localstorage
-			return Promise.resolve(true);
-		} else
-			return Promise.resolve(false);
+		const reply = await postData(form_data);
+
+		// user may have a invalid link, need to display error message (if defined)
+		if (!reply.display) {
+			if (reply.message)
+				DOCUMENT_MAIN.innerHTML = DOMPurify.sanitize(reply.message);
+			//DOCUMENT_MAIN.style.visibility = "visible";
+			return
+		}
+
+		history.replaceState(null, '', window.location.href.split('?')[0]);
+
+		// User is logged in, store new token in localStorage
+		localStorage.setItem("logged_in_token", reply.message);
+
+		await displayMemberArea();
 	}
 
-	isEmailTokenValid().then(valid => {
-		// If email token is valid, redirect to member area
-		if (valid) {
-			(async () => {
-				const FORM_DATA = new FormData();
-				FORM_DATA.append('script', "members.php");
-				FORM_DATA.append('token', localStorage.getItem(CLIENT_TOKEN_NAME) || "");
-				const REPLY = await postData(FORM_DATA);
-				if (REPLY.display)
-					DOCUMENT_MAIN.innerHTML = REPLY.message;	//	will be the members area HTML
-			})().catch(error => {
-				PRODUCTION || console.error("Error fetching members.php:", error);
-			});
-		}
-		else
-			history.replaceState(null, '', window.location.href.split('?')[0]);
-	});
+	//=============================================================
+	// Fetch and display member area
+	//=============================================================
+	async function displayMemberArea(): Promise<void> {
+		//const cookie = getCookie(COOKIE_NAME);
+		//if (!cookie) return false;
 
-	//=============================================================
-	// Prevent form resubmission on page reload
-	//=============================================================
-	if (window.history.replaceState)
-		window.history.replaceState(null, '', window.location.href.split('?')[0]);
+		const form_data = new FormData();
+		form_data.append('script', "members.php");
+		//form_data.append('token', cookie);
+
+		const reply = await postData(form_data);
+
+		if (reply.display) {
+			DOCUMENT_MAIN.innerHTML = DOMPurify.sanitize(reply.message);
+			//DOCUMENT_MAIN.style.visibility = "visible";
+			return;
+		}
+
+		return;
+	}
 
 	//=============================================================
 	// Get CSRF token from server
 	//=============================================================
-	function getCSRFToken() {
-		(async () => {
-			const FORM_DATA = new FormData();
-			FORM_DATA.append('script', "get_csrf_token.php");
-			const REPLY = await postData(FORM_DATA);
+	//function getCSRFToken() {
+	//	(async () => {
+	//		const form_data = new FormData();
+	//		form_data.append('script', "get_csrf_token.php");
+	//		const reply = await postData(form_data);
 
-			if (REPLY.display) {
-				DOCUMENT_CSRF_TOKEN.value = REPLY.message;
-				DOCUMENT_EMAIL.value = "";
-				DOCUMENT_EMAIL.focus();
-			} else {
-				DOCUMENT_MAIN.innerHTML = REPLY.message;
-			}
-		})().catch(error => {
-			PRODUCTION || console.error("Error fetching CSRF token:", error);
-			DOCUMENT_MAIN.textContent = 'Internal error 2';
-		});
-		DOCUMENT_MAIN.style.visibility = "visible";
-	}
+	//		if (reply.display) {
+	//			DOCUMENT_CSRF_TOKEN.value = reply.message;
+	//			DOCUMENT_EMAIL.value = "";
+	//			DOCUMENT_EMAIL.focus();
+	//		} else
+	//			DOCUMENT_MAIN.textContent = reply.message;
+	//		DOCUMENT_MAIN.style.visibility = "visible";
+
+	//	})()
+	//}
 
 	//=============================================================
 	// Email form submit event listener
@@ -169,29 +165,45 @@
 	DOCUMENT_FORM.addEventListener('submit', (event) => {
 		event.preventDefault();
 		(async () => {
-			const FORM_DATA = new FormData();
-			FORM_DATA.append('script', "email_entered.php");
-			FORM_DATA.append('email', DOCUMENT_EMAIL.value);
-			FORM_DATA.append('csrf_token', DOCUMENT_CSRF_TOKEN.value);
-			const REPLY = await postData(FORM_DATA);
-			if (REPLY.display)
-				DOCUMENT_MESSAGE.textContent = "If the email you entered is in our system, you will receive an email with instructions on how to access the member section of this website.";
-			else
-				DOCUMENT_MESSAGE.textContent = REPLY.message;
-		})().catch(_error => {
-			DOCUMENT_MESSAGE.textContent = 'Error processing email entry';
-		}).finally(() => {
-			disableEmailInput();
-		})
+			const form_data = new FormData();
+			form_data.append('script', "email_entered.php");
+			form_data.append('email', DOCUMENT_EMAIL.value);
+			//form_data.append('csrf_token', DOCUMENT_CSRF_TOKEN.value);
+			const reply = await postData(form_data);
+			if (reply.display)
+				DOCUMENT_MAIN.innerHTML = DOMPurify.sanitize("If the email you entered is in our system, you will receive an email with instructions on how to access the member section of this website.");
+			else if (reply.message)
+				DOCUMENT_MAIN.innerHTML = DOMPurify.sanitize(reply.message);
+		})()
 	});
 
 	//=============================================================
 	// Disable email input and remove submit button
 	//=============================================================
-	function disableEmailInput() {
-		DOCUMENT_EMAIL.disabled = true;
-		DOCUMENT_FORM_BUTTON.remove();
-	}
+	//function disableEmailInput() {
+	//	DOCUMENT_EMAIL.disabled = true;
+	//	DOCUMENT_FORM_BUTTON.remove();
+	//}
+
+	//=============================================================
+	// Function to get cookie value by name
+	//=============================================================
+	//function getCookie(name: string): string | undefined {
+	//	const value = `; ${document.cookie}`;
+	//	const parts = value.split(`; ${name}=`);
+	//	if (parts.length === 2)
+	//		return decodeURIComponent(parts.pop()?.split(';').shift() || '');
+
+	//	return undefined;
+	//}
+
+	// Initial actions
+	sendTimezone();
+	isUserLoggedIn();
+	isEmailTokenValid();
+	//getCSRFToken();
+	LOADER.style.display = "none";
+	DOCUMENT_MAIN.style.visibility = "visible";
 })();
 
 export { }
