b19d08c8ef9d2a9ee7f02f34d1a757674b1a4584
Chris Strawser <mcochris@users.noreply.github.com>
2025-10-07 04:01:23 -0400

    See comments for index.ts

--- public/js/index.js
+++ public/js/index.js
@@ -2,14 +2,10 @@
 // Main script
 //=============================================================
 (function () {
-    const TIMEZONE = Intl.DateTimeFormat().resolvedOptions().timeZone;
-    //const DOCUMENT_CSRF_TOKEN = document.querySelector("#csrf-token") as HTMLInputElement;
     const DOCUMENT_FOOTER_YEAR = document.querySelector("#current-year");
     const DOCUMENT_MAIN = document.querySelector("main");
     const DOCUMENT_FORM = document.querySelector("form");
     const DOCUMENT_EMAIL = document.querySelector("#email");
-    //const DOCUMENT_FORM_BUTTON = document.querySelector("#form-button") as HTMLButtonElement;
-    //const COOKIE_NAME = "PHPSESSID";
     const LOADER = document.querySelector(".loader");
     // Set current year in page footer
     DOCUMENT_FOOTER_YEAR.textContent = new Date().getFullYear().toString();
@@ -32,16 +28,12 @@
     async function sendTimezone() {
         const form_data = new FormData();
         form_data.append('script', "set_tz.php");
-        form_data.append('timezone', TIMEZONE);
-        const reply = await postData(form_data);
-        if (!reply.display) {
-            DOCUMENT_MAIN.innerHTML = DOMPurify.sanitize(reply.message);
-            //DOCUMENT_MAIN.style.visibility = "visible";
-        }
+        form_data.append('timezone', Intl.DateTimeFormat().resolvedOptions().timeZone);
+        await postData(form_data);
     }
     ;
     //=============================================================
-    // Check if user is already logged in via browser localStorage
+    // Check if user is already logged in
     //=============================================================
     async function isUserLoggedIn() {
         const logged_in_token = localStorage.getItem("logged_in_token");
@@ -56,13 +48,6 @@
             localStorage.removeItem("logged_in_token");
             return;
         }
-        if (loginReply.message) {
-            // User is logged, should return json array of first & last name
-            const userInfo = JSON.parse(loginReply.message);
-            DOCUMENT_MAIN.innerHTML = DOMPurify.sanitize(`Welcome back ${userInfo.firstName} ${userInfo.lastName}`);
-        }
-        else
-            DOCUMENT_MAIN.innerHTML = DOMPurify.sanitize("Welcome back");
         // User is logged in, fetch member area
         await displayMemberArea();
     }
@@ -77,53 +62,18 @@
         const form_data = new FormData();
         form_data.append('script', "is_email_token_valid.php");
         form_data.append('token', email_token);
+        history.replaceState(null, '', window.location.href.split('?')[0]);
         const reply = await postData(form_data);
-        // user may have a invalid link, need to display error message (if defined)
+        // Token was not legitimate, display message from server (old token, already used, etc.)
         if (!reply.display) {
             if (reply.message)
                 DOCUMENT_MAIN.innerHTML = DOMPurify.sanitize(reply.message);
-            //DOCUMENT_MAIN.style.visibility = "visible";
             return;
         }
-        history.replaceState(null, '', window.location.href.split('?')[0]);
-        // User is logged in, store new token in localStorage
+        // User email token was legitimate, store new token in localStorage
         localStorage.setItem("logged_in_token", reply.message);
         await displayMemberArea();
     }
-    //=============================================================
-    // Fetch and display member area
-    //=============================================================
-    async function displayMemberArea() {
-        //const cookie = getCookie(COOKIE_NAME);
-        //if (!cookie) return false;
-        const form_data = new FormData();
-        form_data.append('script', "members.php");
-        //form_data.append('token', cookie);
-        const reply = await postData(form_data);
-        if (reply.display) {
-            DOCUMENT_MAIN.innerHTML = DOMPurify.sanitize(reply.message);
-            //DOCUMENT_MAIN.style.visibility = "visible";
-            return;
-        }
-        return;
-    }
-    //=============================================================
-    // Get CSRF token from server
-    //=============================================================
-    //function getCSRFToken() {
-    //	(async () => {
-    //		const form_data = new FormData();
-    //		form_data.append('script', "get_csrf_token.php");
-    //		const reply = await postData(form_data);
-    //		if (reply.display) {
-    //			DOCUMENT_CSRF_TOKEN.value = reply.message;
-    //			DOCUMENT_EMAIL.value = "";
-    //			DOCUMENT_EMAIL.focus();
-    //		} else
-    //			DOCUMENT_MAIN.textContent = reply.message;
-    //		DOCUMENT_MAIN.style.visibility = "visible";
-    //	})()
-    //}
     //=============================================================
     // Email form submit event listener
     //=============================================================
@@ -133,36 +83,30 @@
             const form_data = new FormData();
             form_data.append('script', "email_entered.php");
             form_data.append('email', DOCUMENT_EMAIL.value);
-            //form_data.append('csrf_token', DOCUMENT_CSRF_TOKEN.value);
             const reply = await postData(form_data);
             if (reply.display)
                 DOCUMENT_MAIN.innerHTML = DOMPurify.sanitize("If the email you entered is in our system, you will receive an email with instructions on how to access the member section of this website.");
             else if (reply.message)
+                //	Display error message from server (lockout, invalid email, etc.)
                 DOCUMENT_MAIN.innerHTML = DOMPurify.sanitize(reply.message);
         })();
     });
     //=============================================================
-    // Disable email input and remove submit button
+    // Fetch and display member area
     //=============================================================
-    //function disableEmailInput() {
-    //	DOCUMENT_EMAIL.disabled = true;
-    //	DOCUMENT_FORM_BUTTON.remove();
-    //}
+    async function displayMemberArea() {
+        const form_data = new FormData();
+        form_data.append('script', "members.php");
+        const reply = await postData(form_data);
+        if (reply.display)
+            DOCUMENT_MAIN.innerHTML = DOMPurify.sanitize(reply.message);
+    }
     //=============================================================
-    // Function to get cookie value by name
-    //=============================================================
-    //function getCookie(name: string): string | undefined {
-    //	const value = `; ${document.cookie}`;
-    //	const parts = value.split(`; ${name}=`);
-    //	if (parts.length === 2)
-    //		return decodeURIComponent(parts.pop()?.split(';').shift() || '');
-    //	return undefined;
-    //}
     // Initial actions
+    //=============================================================
     sendTimezone();
     isUserLoggedIn();
     isEmailTokenValid();
-    //getCSRFToken();
     LOADER.style.display = "none";
     DOCUMENT_MAIN.style.visibility = "visible";
 })();
