c032d6fa601d3ddf493242562ad58d13b593a20a
Chris Strawser <mcochris@users.noreply.github.com>
2025-10-11 04:16:08 -0400

    See index.ts

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,11 @@
 //=============================================================
 // Main script
 //=============================================================
-(function () {
+(async function () {
     const DOCUMENT_FOOTER_YEAR = document.querySelector("#current-year");
     const DOCUMENT_MAIN = document.querySelector("main");
     const DOCUMENT_FORM = document.querySelector("form");
     const DOCUMENT_EMAIL = document.querySelector("#email");
-    const LOADER = document.querySelector(".loader");
     // Set current year in page footer
     DOCUMENT_FOOTER_YEAR.textContent = new Date().getFullYear().toString();
     //=============================================================
@@ -26,42 +25,37 @@
     // Send timezone to server
     //=============================================================
     async function sendTimezone() {
-        const form_data = new FormData();
-        form_data.append('script', "set_tz.php");
-        form_data.append('timezone', Intl.DateTimeFormat().resolvedOptions().timeZone);
-        await postData(form_data);
+        const form = new FormData();
+        form.append('script', "set_tz.php");
+        form.append('timezone', Intl.DateTimeFormat().resolvedOptions().timeZone);
+        await postData(form);
     }
     ;
     //=============================================================
     // Check if user is already logged in
     //=============================================================
     async function isUserLoggedIn() {
-        const loginForm = new FormData();
-        loginForm.append('script', "is_user_logged_in.php");
-        const loginReply = await postData(loginForm);
-        if (loginReply.display)
-            await displayMemberArea();
+        const form = new FormData();
+        form.append('script', "is_user_logged_in.php");
+        const reply = await postData(form);
+        return reply.status;
     }
     //=============================================================
-    // Check if user clicked on login link in email
+    // Check if user clicked the magic link in email
     //=============================================================
-    async function isEmailTokenValid() {
-        const url_params = new URLSearchParams(window.location.search);
-        const email_token = url_params.get('token');
-        if (!email_token)
-            return;
-        const form_data = new FormData();
-        form_data.append('script', "is_email_token_valid.php");
-        form_data.append('token', email_token);
+    async function isEmailTokenValid(email_token) {
         history.replaceState(null, '', window.location.href.split('?')[0]);
-        const reply = await postData(form_data);
+        const form = new FormData();
+        form.append('script', "is_email_token_valid.php");
+        form.append('token', email_token);
+        const reply = await postData(form);
         // Token was not legitimate, display message from server (old token, already used, etc.)
-        if (!reply.display) {
+        if (!reply.status) {
             if (reply.message)
                 DOCUMENT_MAIN.innerHTML = DOMPurify.sanitize(reply.message);
-            return;
+            return false;
         }
-        await displayMemberArea();
+        return reply.status;
     }
     //=============================================================
     // User clicked the email submit button
@@ -73,7 +67,7 @@
             form_data.append('script', "email_entered.php");
             form_data.append('email', DOCUMENT_EMAIL.value);
             const reply = await postData(form_data);
-            if (reply.display)
+            if (reply.status)
                 DOCUMENT_MAIN.innerHTML = DOMPurify.sanitize("If the email you entered is in our system, you will receive an email with instructions on how to access the member section of this website.");
             else if (reply.message)
                 //	Display error message from server (lockout, invalid email, etc.)
@@ -87,16 +81,21 @@
         const form_data = new FormData();
         form_data.append('script', "members.php");
         const reply = await postData(form_data);
-        if (reply.display)
-            DOCUMENT_MAIN.innerHTML = DOMPurify.sanitize(reply.message);
+        DOCUMENT_MAIN.innerHTML = DOMPurify.sanitize(reply.message);
     }
     //=============================================================
     // Initial actions
     //=============================================================
     sendTimezone();
-    isUserLoggedIn();
-    isEmailTokenValid();
-    LOADER.style.display = "none";
+    const url_params = new URLSearchParams(window.location.search);
+    const email_token = url_params.get('token');
+    if (await isUserLoggedIn())
+        displayMemberArea();
+    else if (email_token) {
+        const reply = await isEmailTokenValid(email_token);
+        if (reply)
+            displayMemberArea();
+    }
     DOCUMENT_MAIN.style.visibility = "visible";
 })();
 export {};
