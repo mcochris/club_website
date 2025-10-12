5a5e220f3a59ed7fb2f021849f61dc3d9cd30638
Chris Strawser <mcochris@users.noreply.github.com>
2025-10-12 16:12:54 -0400

    Added visits table support

--- private/php/members.php
+++ private/php/members.php
@@ -14,4 +14,22 @@ mySessionStart();
 //==============================================================================
 date_default_timezone_set($_SESSION["TZ"] ?? "UTC");
 
+if(empty($_SESSION["user_id"])) {
+	sendResponse(false, "Not logged in");
+	exit;
+}
+
+//==============================================================================
+//	Updated visits table in DB with this visit
+//==============================================================================
+$pdo = openDb();
+try {
+	$stmt = $pdo->prepare("INSERT INTO visits (user_id, ip_address) VALUES (:user_id, :ip_address)");
+	$stmt->bindParam(':user_id', $_SESSION["user_id"], PDO::PARAM_INT);
+	$stmt->bindParam(':ip_address', $_SERVER["REMOTE_ADDR"], PDO::PARAM_STR);
+	$stmt->execute();
+} catch (PDOException $e) {
+	internalError("Database error: " . $e->getMessage());
+}
+
 sendResponse(true, "Hello from the members area");
