cc676a5df561c8fa2a24c01078a9a4438c7f3195
Chris Strawser <mcochris@users.noreply.github.com>
2025-10-17 00:52:30 -0400

    Better?

--- private/php/api.php
+++ private/php/api.php
@@ -7,7 +7,7 @@ $script = trim(filter_input(INPUT_POST, 'script', FILTER_SANITIZE_FULL_SPECIAL_C
 if (empty($script))
 	exit;
 
-require __DIR__ . "/$script";
+require_once __DIR__ . "/$script";
 
 // If we get to here, the script was included and ran without throwing an exception or exiting.
 // Normally if the script returns anything, it does so by calling sendResponse().
